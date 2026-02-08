ea5da530d5e2ac0c05dd6ba5f0f73be1320dc806
mateusTegue <teusteguetorres2001@gmail.com>
2026-02-07 22:03:36 -0500

    Implement blog app structure: add models, serializers, views, and URLs; integrate CKEditor for content editing; update requirements and settings

--- core/settings.py
+++ core/settings.py
@@ -49,9 +49,18 @@ PROJECT_APPS = [
 THIRD_PARTY_APPS = [
     'rest_framework',
     'channels',
+    'django_ckeditor_5',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+CKEDITOR_5_CONFIGS = {
+    'default': { 
+        'toolbar': 'full', 
+        'autoParagraph': False
+    }
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
